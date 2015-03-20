b0d6554d4a9c521d511e912e821c316132505df4
archy <a@0a.io>
2015-03-21 01:24:44 +0800

    global var disabled in harmony (forced 'use strict'): got around it with export var

--- test/basic.js
+++ test/basic.js
@@ -1,5 +1,5 @@
 Tinytest.add('init & insert',function(test,complete){
-DB_Butler.prepare("books");
+DB_Butler.prepares("books");
 var result = DB_Butler.insert({"author":"Archy"});
 console.log(result);
 //test.equal();
