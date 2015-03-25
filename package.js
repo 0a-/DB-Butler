9874e4bbc3b4888f474ef65a824a22ec139eb9ad
0a- <a@0a.io>
2015-03-25 12:12:20 +0800

    added some tests, debugged, refactored, improved code performance, made major changes (self-proclaimed, or at least that is how it felt)

--- package.js
+++ package.js
@@ -13,10 +13,10 @@ Package.onUse(function(api) {
     api.use('mongo', both);
     api.addFiles('lib/someFunctionsThatHelp.next.js', both);
     api.addFiles('lib/DBOperations/readonlyOperations.js', both);
-    api.addFiles('lib/DBOperations/mutableOperations.js', both);
+    api.addFiles('lib/DBOperations/mutableOperations.next.js', both);
     api.addFiles('lib/query.next.js', both);
     api.addFiles('lib/xToMany.next.js', both);
-    api.addFiles('lib/butlerObjects.js', both);
+    api.addFiles('lib/butlerObjects.next.js', both);
     api.addFiles('lib/DBButler.next.js', both);
     api.addFiles('final.js', both);
     api.export('DBButler',both);
@@ -26,5 +26,6 @@ Package.onTest(function(api) {
     api.use('arch:db-butler');
     api.use('tinytest');
     api.addFiles('test/init.js','server');
-    api.addFiles('test/basic.js','client');
+    api.addFiles('test/basicDBOperations.js','client');
+    api.addFiles('test/advanceDBOperations.js','client');
 });
--- test/basicDBOperations.js
+++ test/basicDBOperations.js
@@ -2,11 +2,9 @@
 Tinytest.add('count', function(test){
   var col = DBButler.prepare("books");
   DBButler.insert({a:"asd"});
-
   var c = DBButler.count("books",{});
   test.equal(col.find().count(),c);
   DBButler.insert([{a:"asd"},{a:"asd"},{a:"asd"}]);
-
   var d = DBButler.count();
   test.equal(d,c+3);
 });
@@ -18,17 +16,13 @@ Tinytest.add('find & get', function(test){
   test.equal(t[0],z[0]);
   z1 = DBButler.getOne({a:"b"});
    z1 = DBButler.getOne({a:"b"});
-  console.log(z[0],z1);
   test.equal(z[0].a,z1.a);
 });
 
 Tinytest.add('update', function(test){
-   DBButler.insert({a:"b"});
+  var id =DBButler.insert({a:"b"});
   var start = DBButler.count({a:"x"});
-  console.log(DBButler.getOne({a:"b"}));
-  var id = DBButler.getOne({a:"b"})._id;
   var z2 = DBButler.edit({_id:id},{a:"x"});
   var end = DBButler.count({a:"x"});
-  console.log(DBButler.getOne({_id:id}));
   test.equal(start+1,end);
 });
